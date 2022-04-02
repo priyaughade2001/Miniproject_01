0f3f6934da71c4842cdb6f3ba735facd77ddfc13
Priya P. Ughade <90169620+priyaughade2001@users.noreply.github.com>
2022-04-03 01:21:37 +0530

    Update test_project.c

--- 3_Implementation/test/test_project.c
+++ 3_Implementation/test/test_project.c
@@ -1,14 +1,37 @@
+
 #include "unity.h"
 
-void setUp(void) { };
-void tearDown(void) { };
+int currentPoints = 0;
+int maxPoints = 100;
 
-int main(void) 
+//UUTime tests
+
+TEST(UUTime, TestTimeConstructor1)
 {
-    UnityBegin(NULL);
+	std::cout << "\nRunning test TestConstructor 1" << std::endl;
 
-    UnityDefaultTestRun(, "", 10);
+	//Arrange
+	int expected_result{10};
+	int actual_result {0};
+		
 
-    return (UnityEnd());
+	//ACT
+	UUTime newTime{};
+	actual_result = newTime.GetHours();
 
+
+	//Assert
+	ASSERT_EQ(expected_result, actual_result);
+
+	if (!HasFailure()) {
+		//Test has passed
+		currentPoints += 1; //add points
+		std::cout << "Test UUTime constructor PASSED! : The default UUTime constructor sets the time correctly" << std::endl;
+
+	}
+	else if (HasFailure()) {
+		//Test failed - provide feedback
+		std::cout << "Test UUTime constructor FAILED! : The default UUTime constructor doesn't set the time correctly " << std::endl;
+	}
+	std::cout << "Current MARKS for asssignment 2: " << currentPoints << "/" << maxPoints << "\n" << std::endl;
 }
